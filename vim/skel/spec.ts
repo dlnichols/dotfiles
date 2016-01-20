6d3eebe2ab739e1888a57e1e08b411130b012225
Dan Nichols <daniel@iruleathome.com>
2016-01-20 15:54:38 -0500

    Fix skel styling and add some irule env

--- vim/skel/spec.ts
+++ vim/skel/spec.ts
@@ -1,9 +1,9 @@
 import { it, iit, xit, describe, ddescribe, xdescribe, expect } from 'angular2/testing';
 
 describe('class', () => {
-    describe('function', () => {
-        // Simple functionality tests
+	describe('function', () => {
+		// Simple functionality tests
 
-        // Invalid input tests
-    });
+		// Invalid input tests
+	});
 });
--- zprofile
+++ zprofile
@@ -1,6 +1,7 @@
 typeset -U path
 path=($path[@] ~/bin ~/.rvm/bin ~/Android/Sdk/platform-tools)
 export EDITOR="vim"
+export IRULE_TEST_BUILDER_LOCAL=true
 
 # Load RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
